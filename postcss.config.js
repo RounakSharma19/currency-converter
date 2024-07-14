ddd5a25dfb786ba51e46351b3107f02e50c15e08
RounakSharma19 <rounaksharma808@gmail.com>
2024-07-15 01:27:03 +0530

    fix: pipeline build fail issue

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const config = require("./tailwind.config");
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: { config },
-    autoprefixer: {},
-  },
-};
